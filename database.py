5afc9a5e1ddafd7e6979d5ebdec8c2bb00ae3554
kdsouza <keegan0415dsouza@gmail.com>
2019-01-26 13:59:17 -0800

    added database file

---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+import uuid
+
+
+conn = sqlite3.connect("C:\\Users\\Keegan\\Desktop\\dbFolder\\meetings.db")
+
+
+def insert_meeting(name, date, time, location, description):
+    id = str(uuid.uuid1())
+    if len(name) == 0:
+        raise ValueError
+    if len(date) == 0:
+        raise ValueError
+    meeting = [(id, name, date, time, location, description)]
+    cursor = conn.cursor()
+    cursor.executemany("INSERT INTO meetings VALUES(?, ?, ?, ?, ?, ?)", meeting)
+    conn.commit()
+    return id
+
+def get_meeting(id):
+    t = (id,)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM meetings WHERE ID=?" , t)
+    return cursor.fetchone()
+
+
+
+if __name__ == "__main__":
+    try:
+        id = insert_meeting("This is another test", "Hello", "01/13/12", "1957 Avanti Way", "This is a test")
+    except Exception as e:
+    print("There was an error inserting into the database")
+
+    try:
+        meeting = get_meeting(id)
+    except Exception:
+        print("Cannot get meeting")
+
+
