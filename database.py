1329267a8f8c1cf4b0ad578d2552e784bff99a03
kdsouza <keegan0415dsouza@gmail.com>
2019-01-26 15:06:00 -0800

    Implemented basic database functionally, also fixed CORS issue

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ import sqlite3
 import uuid
 
 
-conn = sqlite3.connect("C:\\Users\\Keegan\\Desktop\\dbFolder\\meetings.db")
 
 
 def insert_meeting(name, date, time, location, description):
@@ -14,13 +13,16 @@ def insert_meeting(name, date, time, location, description):
     if len(date) == 0:
         raise ValueError
     meeting = [(id, name, date, time, location, description)]
-    cursor = conn.cursor()
-    cursor.executemany("INSERT INTO meetings VALUES(?, ?, ?, ?, ?, ?)", meeting)
-    conn.commit()
-    return id
+    
+    with sqlite3.connect("C:\\Users\\Keegan\\Desktop\\dbFolder\\meetings.db") as conn:
+        cursor = conn.cursor()
+        cursor.executemany("INSERT INTO meetings VALUES(?, ?, ?, ?, ?, ?)", meeting)
+        conn.commit()
+        return id
 
 def get_meeting(id):
     t = (id,)
+    conn = sqlite3.connect("C:\\Users\\Keegan\\Desktop\\dbFolder\\meetings.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM meetings WHERE ID=?" , t)
     return cursor.fetchone()
@@ -31,7 +33,7 @@ if __name__ == "__main__":
     try:
         id = insert_meeting("This is another test", "Hello", "01/13/12", "1957 Avanti Way", "This is a test")
     except Exception as e:
-    print("There was an error inserting into the database")
+        print("There was an error inserting into the database")
 
     try:
         meeting = get_meeting(id)
--- helloworld.py
+++ helloworld.py
@@ -4,10 +4,14 @@ from email.message import EmailMessage
 import urllib.parse as urlparse
 from urllib.parse import urlencode
 
-from flask import Flask, request, redirect, abort
-from emailer import sendTestEmail
+from flask import Flask, request, redirect, abort, jsonify
+from flask_cors import CORS
+import database
+
+
 
 app = Flask(__name__)
+CORS(app)
  
 URL = "http://127.0.0.1:5500/"
 INVITATION_PATH = "invitation.html"
@@ -25,6 +29,8 @@ class meeting:
         url_parts = list(urlparse.urlparse(URL + INVITATION_PATH))
         url_parts[4] = urlencode(params)
         return urlparse.urlunparse(url_parts)
+
+   
     
     def __str__(self):
         return 'name:\t' + str(self.name) + \
@@ -33,7 +39,14 @@ class meeting:
                '\nlocation:\t' + str(self.location) + \
                '\ndescriptions:\t' + str(self.description)  
        
-      
+def get_url_for_id(id):
+    params = {'id': id}
+    url_parts = list(urlparse.urlparse(URL + INVITATION_PATH))
+    url_parts[4] = urlencode(params)
+    return urlparse.urlunparse(url_parts)
+
+
+
 meetings = []
 
 @app.route('/')
@@ -47,14 +60,30 @@ def add_meeting():
     date = request.form['date']
     time = request.form['time']
     location = request.form['location']
-    m = meeting(name, date, time, location, description)
-    return redirect(m.get_url())
+    #m = meeting(name, date, time, location, description)
+    id = database.insert_meeting(name, date, time, location, description)
+    
+    return redirect(get_url_for_id(id))
+
+@app.route('/getMeeting' , methods = ['GET'])
+def get_meeting():
+    meeting_id = request.args["id"]
+    meeting_tuple = database.get_meeting(meeting_id)
+    print(meeting)
+    meeting_data = {}
+    meeting_data['name'] = meeting_tuple[1]
+    meeting_data['date'] = meeting_tuple[2]
+    meeting_data['time'] = meeting_tuple[3]
+    meeting_data['location'] = meeting_tuple[4]
+    meeting_data['description'] = meeting_tuple[5]
+    return jsonify(meeting_data)
+
+
 
 @app.route('/email', methods = ['POST'])
 def email_stuff():
     print("Hello There")
-    sendTestEmail()
-    return "hello"
+    return "Ingore not implemented yet"
 
 
 
